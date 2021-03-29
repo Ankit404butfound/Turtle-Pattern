8d5793fc34b3f839b1c2c983759c58adc36d1dc1
Ankan Mahapatra <69568975+ankan2526@users.noreply.github.com>
2021-03-29 15:16:24 +0530

    Update main.py
    Just trying to learn how github works.

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ b=0
 
 turtle.bgcolor("black")
 turtle.speed(0)
-#turtle.pencolor("blue")
 screen=Screen()
 screen.colormode(255) 
 turtle.pencolor(250,0,0)
@@ -17,9 +16,7 @@ turtle.pendown()
 while True:
     turtle.pencolor(250-(b),50,50+b)
     turtle.forward(a)
-    #turtle.backward(a)
     turtle.right(b)
-    #turtle.left(b)
 
     a+=3
     b+=1
